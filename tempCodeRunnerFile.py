e13c557ef066bceffb692bc6f6519ab5fca32e9c
Mohansharma13 <msharma13613@gmail.com>
2024-06-18 22:21:51 +0530

    Initial commit
    Former-commit-id: 4e1b1748ab84cd78cd8a8ceb123deb9f0c8b384d

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_tags import st_tags
+
+
+st.title('Movie Recommender System')
+
+keywords = st_tags(
+    label='# Enter Keywords:',
+    text='Press enter to add more',
+    value=['Zero', 'One', 'Two'],
+    suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
+    maxtags = 4,
+    key='1')
+print(keywords)
